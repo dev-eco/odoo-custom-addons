efb7b8e0e2cf148bfba76b026104e6f15c51eefd
Dev-Eco <admin@ecocaucho.org>
2025-10-10 10:57:02 +0200

    Fix view document...

---
+++ document_automation/static/src/js/document_preview.js
@@ -0,0 +1,166 @@
+/**
+ * Módulo de Previsualización de Documentos
+ * 
+ * Este módulo extiende la funcionalidad estándar de Odoo para permitir
+ * la previsualización de documentos escaneados directamente en la interfaz.
+ */
+odoo.define('document_automation.preview', function (require) {
+    "use strict";
+
+    // Importamos las dependencias necesarias de Odoo
+    var core = require('web.core');
+    var Widget = require('web.Widget');
+    var FieldBinary = require('web.basic_fields').FieldBinary;
+    var registry = require('web.field_registry');
+    var utils = require('web.utils');
+    
+    // Traducción
+    var _t = core._t;
+    var QWeb = core.qweb;
+
+    /**
+     * Widget de Previsualización de Documentos
+     * 
+     * Este widget extiende el campo binario estándar añadiendo capacidades
+     * de previsualización para documentos PDF e imágenes comunes.
+     */
+    var DocumentPreviewWidget = FieldBinary.extend({
+        template: 'DocumentPreview',
+        supportedTypes: ['application/pdf', 'image/jpeg', 'image/png', 'image/gif', 'image/tiff'],
+        events: _.extend({}, FieldBinary.prototype.events, {
+            'click .o_document_preview_button': '_onClickPreview',
+        }),
+        
+        /**
+         * Inicializa el widget de previsualización.
+         * Configura las opciones de visualización y determina el tipo de documento.
+         */
+        init: function (parent, name, record, options) {
+            this._super.apply(this, arguments);
+            this.previewEnabled = true;
+            this.documentType = 'unknown';
+            this.filename = record.data.filename || '';
+            
+            // Determina el tipo de documento basado en la extensión del archivo
+            if (this.filename) {
+                var extension = this.filename.split('.').pop().toLowerCase();
+                if (['pdf'].includes(extension)) {
+                    this.documentType = 'pdf';
+                } else if (['jpg', 'jpeg', 'png', 'gif', 'bmp'].includes(extension)) {
+                    this.documentType = 'image';
+                }
+            }
+        },
+        
+        /**
+         * Renderiza el widget y añade botones de previsualización cuando es aplicable.
+         * 
+         * @override
+         */
+        _render: function () {
+            // Primero renderizamos el campo binario normal
+            this._super.apply(this, arguments);
+            
+            // Si tenemos un valor y es un tipo que podemos previsualizar
+            if (this.value && this.previewEnabled && (this.documentType === 'pdf' || this.documentType === 'image')) {
+                // Añadimos el botón de previsualización después del botón de descarga
+                var $previewButton = $('<button>', {
+                    class: 'btn btn-sm btn-primary o_document_preview_button ml-2',
+                    type: 'button',
+                    text: _t('Vista previa')
+                });
+                
+                this.$el.find('.o_clear_file_button').after($previewButton);
+            }
+        },
+        
+        /**
+         * Maneja el evento de clic en el botón de previsualización.
+         * Abre un diálogo modal con el documento incrustado.
+         * 
+         * @private
+         */
+        _onClickPreview: function () {
+            var self = this;
+            var fileData = this.value;
+            
+            if (!fileData) {
+                return; // No hay datos para previsualizar
+            }
+            
+            // Creamos el diálogo modal que contendrá la previsualización
+            var $content = $('<div>', {
+                class: 'o_document_preview_container'
+            });
+            
+            // Diferentes manejos según el tipo de documento
+            if (this.documentType === 'pdf') {
+                // Para PDF, creamos un objeto embed
+                $content.append($('<embed>', {
+                    src: 'data:application/pdf;base64,' + fileData,
+                    type: 'application/pdf',
+                    width: '100%',
+                    height: '500px'
+                }));
+            } else if (this.documentType === 'image') {
+                // Para imágenes, usamos una etiqueta img
+                $content.append($('<img>', {
+                    src: 'data:image;base64,' + fileData,
+                    class: 'img-fluid',
+                    alt: this.filename
+                }));
+            }
+            
+            // Creamos y abrimos el diálogo modal
+            var dialog = new Dialog(this, {
+                title: _t('Previsualización: ') + this.filename,
+                size: 'large',
+                $content: $content,
+                buttons: [{
+                    text: _t('Cerrar'),
+                    close: true
+                }]
+            });
+            
+            dialog.open();
+        }
+    });
+    
+    // Registramos nuestro widget para que esté disponible en las vistas
+    registry.add('document_preview', DocumentPreviewWidget);
+    
+    /**
+     * Widget para la vista Kanban que muestra miniaturas de documentos
+     */
+    var DocumentKanbanPreview = Widget.extend({
+        template: 'DocumentKanbanPreview',
+        events: {
+            'click .o_document_kanban_preview': '_onClickPreview',
+        },
+        
+        /**
+         * Inicializa el widget para la vista Kanban
+         */
+        init: function (parent, options) {
+            this._super.apply(this, arguments);
+            this.document = options.document || {};
+        },
+        
+        /**
+         * Abre la previsualización cuando se hace clic en la miniatura
+         */
+        _onClickPreview: function (ev) {
+            ev.preventDefault();
+            ev.stopPropagation();
+            
+            // Aquí podríamos implementar una previsualización en miniatura para la vista Kanban
+            // similar a la función _onClickPreview del widget DocumentPreviewWidget
+        }
+    });
+    
+    // Exportamos los widgets para su uso en otras partes del módulo
+    return {
+        DocumentPreviewWidget: DocumentPreviewWidget,
+        DocumentKanbanPreview: DocumentKanbanPreview,
+    };
+});
