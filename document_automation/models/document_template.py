174d50df64d61bbd2d611ce3d1bc4991a10abc82
Dev-Eco <admin@ecocaucho.org>
2025-10-10 10:21:37 +0200

    Fix view document...

---
+++ document_automation/models/document_template.py
@@ -0,0 +1,11 @@
+# models/document_template.py
+from odoo import models, fields, api
+
+class DocumentTemplate(models.Model):
+    _name = 'document.template'
+    _description = 'Plantilla de Documento'
+    
+    name = fields.Char('Nombre', required=True)
+    document_type_id = fields.Many2one('document.type', 'Tipo de documento', required=True)
+    template_content = fields.Text('Contenido de la plantilla')
+    active = fields.Boolean('Activo', default=True)
