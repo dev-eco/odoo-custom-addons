8057083c4ffe8b860f4e8115fbe62a397a13c8e7
Dev-Eco <admin@ecocaucho.org>
2025-10-17 14:06:29 +0200

    fix errors

--- plantilla_factura/__manifest__.py
+++ plantilla_factura/__manifest__.py
@@ -55,7 +55,6 @@
     'data': [
         'security/ir.model.access.csv',
         'views/report_facturas_templates.xml',
-        'views/report_facturas_DEBUG.xml',
         'reports/invoice_reports.xml',
         'views/account_move_views.xml',  # Nuevo archivo para añadir campos en formulario
     ],
